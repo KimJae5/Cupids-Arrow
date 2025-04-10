f9f706366435c723deb8053871239b895c10c82b
KimJae5 <C24404366@mytudublin.ie>
2025-04-10 19:51:45 +0100

    addded music

--- cupid's-arrow/enemy.gd
+++ cupid's-arrow/enemy.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 @onready var player = get_node("/root/Map/Player")
 @export var normal_texture : Texture
 @export var damaged_texture : Texture
- 
+@onready var deathsound = $DeathSound
+
 var entered : bool
 var speed : int = 150
 var direction : Vector2
@@ -37,22 +38,26 @@ var current_health = 0
 func take_damage(amount: int):
 	current_health -= amount
 	update_health_bar()
+	
 
 	if current_health <= 3:
 		show_critical_image()
+		deathsound.play()
 
 	if current_health <= 0:
 		_on_death()
+		
+
 	
 
 func _on_death(): 
-	print("Enemy died!")
 	var main = get_tree().current_scene.get_node("Main")
 	if main and main.has_method("increase_score"):
 		print("Found main scene, increasing score.")
 		main.increase_score(100)
 	else:
 		print("Main scene not found or missing method.")
+
 	queue_free()
 
 func update_health_bar():
--- cupid's-arrow/player.gd
+++ cupid's-arrow/player.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 signal player_shoot(position, direction)
 
+
 const speed = 400
 const ACCEL = 5.0
+@onready var bowshot = $BowShot
 
 var input: Vector2
 
@@ -11,17 +13,18 @@ func get_input():
 	input.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	input.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	return input.normalized()
+
 	
 
 func _physics_process(_delta):
 	var playerInput = get_input()
 	velocity = playerInput.normalized() * speed
-	
 	if Input.is_action_just_pressed("Shoot"):
 		var mouse_pos = get_global_mouse_position()
 		var dir = mouse_pos - global_position
 		emit_signal("player_shoot", global_position, dir)
+		bowshot.play()
 
-
+	
 	move_and_slide()
 	
